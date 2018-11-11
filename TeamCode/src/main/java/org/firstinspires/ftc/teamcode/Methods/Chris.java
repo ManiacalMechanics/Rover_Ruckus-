7d0e03b291319f0985e3b680c42d8b08b41b76fb
RyanXDlord2018forevera <kellyraywilliamsjr@gmail.com>
2018-11-11 15:41:53 -0600

    removed comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/Chris.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/Chris.java
@@ -63,12 +63,6 @@ public class Chris extends LinearOpMode {
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-
-
-
-        //gay
-
-
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
