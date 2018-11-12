d4a37768f53b9aca71e87365ecb45eafe91d0858
RyanXDlord2018forevera <kellyraywilliamsjr@gmail.com>
2018-11-11 21:03:45 -0600

    chris.exe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/Chris.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/Chris.java
@@ -81,33 +81,33 @@ public class Chris extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
+                            // Setup a variable for each drive wheel to save power level for telemetry
+                            double leftPower;
+                            double rightPower;
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
+                            // Choose to drive using either Tank Mode, or POV Mode
+                            // Comment out the method that's not used.  The default below is POV.
 
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+                            // POV Mode uses left stick to go forward, and right stick to turn.
+                            // - This uses basic math to combine motions and is easier to drive straight.
+                            double drive = -gamepad1.left_stick_y;
+                            double turn  =  gamepad1.right_stick_x;
+                            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+                            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
+                            // Tank Mode uses one stick to control each wheel.
+                            // - This requires no math, but it is hard to drive forward slowly and keep straight.
+                            // leftPower  = -gamepad1.left_stick_y ;
+                            // rightPower = -gamepad1.right_stick_y ;
 
-            // Send calculated power to wheels
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
+                            // Send calculated power to wheels
+                            leftDrive.setPower(leftPower);
+                            rightDrive.setPower(rightPower);
 
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-            telemetry.update();
+                            // Show the elapsed game time and wheel power.
+                            telemetry.addData("Status", "Run Time: " + runtime.toString());
+                            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+                            telemetry.update();
         }
     }
 }
