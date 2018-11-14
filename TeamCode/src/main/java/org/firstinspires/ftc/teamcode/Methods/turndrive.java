6972c91862c9a4f37957faaa20614c1aa57de457
RyanXDlord2018forevera <kellyraywilliamsjr@gmail.com>
2018-11-14 00:49:28 -0600

    cleaned a little bit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/turndrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/turndrive.java
@@ -7,17 +7,15 @@ public  class turndrive extends BasicOpMode_Linear{
     double drive = -gamepad1.left_stick_y;
     double turn  =  gamepad1.right_stick_x;
 
-    double rdrive(){
-        Init();
-        double rdrive;
-        rdrive   = Range.clip(drive - turn, -1.0, 1.0) ;
-        return rdrive();
-    }
+            double rdrive(){
+                Init();
+                double rdrive = Range.clip(drive - turn, -1.0, 1.0) ;
+                return rdrive();
+            }
 
-    double ldrive(){
-        Init();
-        double ldrive;
-        ldrive =Range.clip(drive+turn,-1.0,1.0);
-        return ldrive();
-    }
+            double ldrive(){
+                Init();
+                double ldrive = Range.clip(drive+turn,-1.0,1.0);
+                return ldrive();
+            }
 }
