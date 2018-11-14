e4d13050f67a453544e7d983e3eafe8e7fb43a0f
RyanXDlord2018forevera <kellyraywilliamsjr@gmail.com>
2018-11-14 14:06:02 -0600

    uhghh

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/twm.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.Methods;
+
+import java.lang.Math;
+
+public class twm extends robotmanager {
+    void diagSpin() {
+        Init();
+        double time=getRuntime();
+       //Math.sin(time) = rightPower;
+    }
+}
