c4926271abfc18fe47efe0b25f8c5e9b1d3122b0
Jeppe <Jeppebs03@gmail.com>
2018-11-18 14:49:13 -0600

    motor power
    set the power for the motors in the drive methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/driveMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/driveMethods.java
@@ -9,13 +9,19 @@ public  class driveMethods extends robotmanager{
             turn = gamepad1.right_stick_x;
             leftPower = Range.clip(drive + turn, -1.0, 1.0);
             rightPower = Range.clip(drive + turn, -1.0, 1.0);
+            rightDrive.setPower(rightPower);
+            leftDrive.setPower(leftPower);
+
         }
     }
     void driveTank() {
 while(opModeIsActive()){
             leftPower = gamepad1.left_stick_y;
             rightPower = gamepad1.right_stick_y;
+    rightDrive.setPower(rightPower);
+    leftDrive.setPower(leftPower);
 
-    }
+
+}
     }
 }
