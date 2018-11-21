f62244d9b8aa6f3404c9db86ed09360935bc5d57
Chris Allison <chrisallison03@gmail.com>
2018-11-20 17:27:16 -0800

    loljeppesmallpik

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/Chris.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/Chris.java
@@ -81,26 +81,45 @@ public class Chris extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-                            // Setup a variable for each drive wheel to save power level for telemetry
-                            double leftPower;
-                            double rightPower;
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower;
+            double rightPower;
 
-                            // Choose to drive using either Tank Mode, or POV Mode
-                            // Comment out the method that's not used.  The default below is POV.
+            // Choose to drive using either Tank Mode, or POV Mode
+            // Comment out the method that's not used.  The default below is POV.
 
-                            // POV Mode uses left stick to go forward, and right stick to turn.
-                            // - This uses basic math to combine motions and is easier to drive straight.
-                            double drive = -gamepad1.left_stick_y;
-                            double turn  =  gamepad1.right_stick_x;
-                            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-                            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-
-
-
-                            int a==1;
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+            double drive = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+            leftPower = Range.clip(drive + turn, -1.0, 1.0);
+            rightPower = Range.clip(drive - turn, -1.0, 1.0);
 
 
+            int a==1;
+            if (a == 1) {
+                jumpoffabridge
 
+            } else {
+                dontjumpoffabridge
+            }
+            int b==2;
+            if (b == 2) {
+                jeppeisgay
+            } else {
+                jeppeisstillgay
+            }
+            int c==3;
+            if (c == 3) {
+                jeppeisCSGOgod
+            } else {
+                jeppeisstillbasicallycsgogod;
+            }
+        }}}
+        int d==4;
+if(d--4){
+        }jeppehassmallpik
+        else xdxd
 
 
 
