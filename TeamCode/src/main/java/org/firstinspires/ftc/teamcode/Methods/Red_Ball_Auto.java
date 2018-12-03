f6ebda567c2cfab6929d05200f1673a795845d9d
Jeppe <Jeppebs03@gmail.com>
2018-12-03 16:53:04 -0600

    fixed redball auto error
    a bracket was missing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/Red_Ball_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/Red_Ball_Auto.java
@@ -8,13 +8,12 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous(name = "Red_Ball_Auto", group = "Autonomous")
 public class Red_Ball_Auto extends robotmanager {
 
-    robotmanager    robot  = new robotmanager();
-    private ElapsedTime     runtime = new ElapsedTime();
+    robotmanager robot = new robotmanager();
+    private ElapsedTime runtime = new ElapsedTime();
     Object robotmanager;
 
     @Override
     public void runOpMode() {
-
 
 
         telemetry.addData("Status", "Reseting Enconders ");
@@ -24,7 +23,7 @@ public class Red_Ball_Auto extends robotmanager {
         robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Path0",  "Starting at %7d :%7d",
+        telemetry.addData("Path0", "Starting at %7d :%7d",
                 robot.leftDrive.getCurrentPosition(),
                 robot.rightDrive.getCurrentPosition());
         telemetry.update();
@@ -47,5 +46,6 @@ public class Red_Ball_Auto extends robotmanager {
         stop();
 
 
-
+    }
 }
+
