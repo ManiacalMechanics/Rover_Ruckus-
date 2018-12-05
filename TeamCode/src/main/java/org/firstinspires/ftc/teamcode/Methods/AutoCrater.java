b5378aa86f7d5cc74f0eaef263d05af919216e1b
Jeppe <Jeppebs03@gmail.com>
2018-12-04 18:24:30 -0600

    more changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/AutoCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/AutoCrater.java
@@ -14,80 +14,99 @@ public class AutoCrater extends robotmanager {
 
     /*Declare OpMode members */
 
+    static final double     FORWARD_SPEED = 0.6;
+    static final double     TURN_SPEED    = 1;
     private ElapsedTime runtime = new ElapsedTime();
 
 
     @Override
     public void runOpMode() {
-        Init();
+        Init();        // The init() method of the hardware class does all the work here
 
-
-        telemetry.addData("Status", "Reseting Enconders ");
-        telemetry.update();
-
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Path0", "Starting at %7d :%7d",
-                leftDrive.getCurrentPosition(),
-                rightDrive.getCurrentPosition());
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-        telemetry.addData("Mode", "running");
-        telemetry.update();
 
-        // set left motor to run for 5000 encoder counts.
+        //Step 1: move lift motor down
+        while (runtime.seconds() < 5)
+        {
+            liftMotor.setPower(2);
+            telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
 
+        // Step 2:  Spin right for 0.3 seconds
+        runtime.reset();
+        while ( runtime.seconds() < .3)
+        {
+            leftDrive.setPower(TURN_SPEED);
+            rightDrive.setPower(-TURN_SPEED);
+            telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
 
-        // set both motors to 25% power. Movement will start.
-        waitForStart();
+        //        leftDrive.setPower(0);
+        //        rightDrive.setPower(0);
 
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        while (opModeIsActive()) {
-
-            while(liftMotor.getCurrentPosition()<=100)
-            {
-              liftMotor.setPower(1);
-              telemetry.addData("Position",liftMotor.getCurrentPosition());
-              telemetry.update();
-            }
-
-
-
-
-
-            while(liftMotor.getCurrentPosition() >= 100) {
-                liftMotor.setPower(0);
-                while (leftDrive.getCurrentPosition() < 5000 && rightDrive.getCurrentPosition() < 2000) {
-                    leftDrive.setPower(1);
-                    rightDrive.setPower(1);
-                }
-
-                while (rightDrive.getCurrentPosition() >= 2000 && leftDrive.getCurrentPosition() < 5000) {
-                    rightDrive.setPower(0);
-                    leftDrive.setPower(1);
-                }
-                rightDrive.setPower(0);
-                leftDrive.setPower(0);
-            }
+        //Step 3 move forward oh so slightly
+        while (runtime.seconds() < .4)
+        {
+            leftDrive.setPower(FORWARD_SPEED);
+            rightDrive.setPower(FORWARD_SPEED);
+            telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
 
         }
 
+        // Step 4 turn left slightly to straighten up robot
+        while (runtime.seconds() < .7)
+        {
+            leftDrive.setPower(-TURN_SPEED);
+            rightDrive.setPower(TURN_SPEED);
+            telemetry.addData("Path", "leg 2: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
 
+        //Step 5 move straight for the crater
+        while(runtime.seconds() < 0.5){
+            leftDrive.setPower(-FORWARD_SPEED);
+            rightDrive.setPower(-FORWARD_SPEED);
+
+        }
+        runtime.reset();
+
+
+        //step 6: Turn back and to the right
+
+        while(runtime.seconds() < 0.3){
+            leftDrive.setPower(-TURN_SPEED);
+            rightDrive.setPower(TURN_SPEED);
+
+        }
+
+        while (opModeIsActive() && (runtime.seconds() < 1.0))
+        {
+            telemetry.addData("Path", "Leg 3: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+
+        //Step 6 spit out marker
+        boxMotor.setPower(1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2.0))
+        {
+            telemetry.addData("Path", "Leg 3: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
     }
 }
 
