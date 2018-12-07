05770a3191ef6f0e202a6d01e42e712fa63c92cc
Jeppe <Jeppebs03@gmail.com>
2018-12-07 17:18:30 -0600

    fixed an error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/AutoCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/AutoCrater.java
@@ -98,7 +98,7 @@ runtime.reset();
         }
 runtime.reset();
 
-        while (opModeIsActive() && runtime.seconds() < 1.0))
+        while (opModeIsActive() && runtime.seconds() < 1.0)
         {
             telemetry.addData("Path", "Leg 3: %2.5f S Elapsed", runtime.seconds());
             telemetry.update();
