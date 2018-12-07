66a200bcdee64a84f77e3be1d38d08338ec00164
Jeppe <Jeppebs03@gmail.com>
2018-12-07 17:36:41 -0600

    changed time for lift motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/AutoCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/AutoCrater.java
@@ -33,7 +33,7 @@ public class AutoCrater extends robotmanager {
 
         //Step 1: move lift motor down
         runtime.reset();
-        while (opModeIsActive() && runtime.seconds() < 5.8)
+        while (opModeIsActive() && runtime.seconds() < 6)
         {
             liftMotor.setPower(2);
             telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.seconds());
