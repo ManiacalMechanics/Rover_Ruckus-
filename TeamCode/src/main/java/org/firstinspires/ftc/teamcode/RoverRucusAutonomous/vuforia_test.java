b31d127de74d40eb247a37e3c64a8760b3dbc4c9
Jeppe <Jeppebs03@gmail.com>
2018-12-11 19:57:16 -0600

    created a test class for vuforia

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRucusAutonomous/vuforia_test.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.RoverRucusAutonomous;
+package org.firstinspires.ftc.teamcode.RoverRucusAutonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+import org.firstinspires.ftc.teamcode.Methods.robotmanager;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+@Disabled
+@Autonomous(name="Vuforia test", group="testing")
+public class vuforia_test {
+
+
+
+
+
+
+
+
+}
